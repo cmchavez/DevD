69e40e66825bc6b8996265da36618ecc832b7d16
Cristal Chavez <cristalmchavez@yahoo.com>
2015-07-01 12:02:48 -0700

    updates environment file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
- 
+  helper :all # include all helpers, all the time
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  def initialize_wordnik(options={})
+    options.merge!({:api_key=>ENV['WORDNIK_API_KEY']})
+    @wordnik = Wordnik::Wordnik.new(options)
+    @wotd = @wordnik.word_of_the_day
+  end
+
 
 end
