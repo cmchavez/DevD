57a1a9529163e0c2a34c0ce761b0fd61755005e1
Cristal Chavez <cristalmchavez@yahoo.com>
2015-07-01 12:16:06 -0700

    clears app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,6 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  def initialize_wordnik(options={})
-    options.merge!({:api_key=>ENV['WORDNIK_API_KEY']})
-    @wordnik = Wordnik::Wordnik.new(options)
-    @wotd = @wordnik.word_of_the_day
-  end
 
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-config.gem "wordnik"
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ENV['WORDNIK_API_KEY'] = "72f822a10caf5b76405070ac1070691e86b0ba82383543f4d"
+
