63e04ffc63c4cb2d055e32b6fc29c018d71dd548
Daley Meistrell <dstrell14@gmail.com>
2015-07-01 11:42:35 -0700

    adds api key

--- config/initializers/wordnik.rb
+++ config/initializers/wordnik.rb
@@ -1,7 +1,8 @@
 Wordnik.configure do |config|
-  config.api_key = '12345abcde'               # required
-  config.username = 'bozo'                    # optional, but needed for user-related functions
-  config.password = 'cl0wnt0wn'               # optional, but needed for user-related functions
-  config.response_format = 'json'             # defaults to json, but xml is also supported
-  config.logger = Logger.new('/dev/null')     # defaults to Rails.logger or Logger.new(STDOUT). Set to Logger.new('/dev/null') to disable logging.
+	config.api_key = ENV["WORDNIK_API_KEY"]
+  # config.api_key = '12345abcde'               # required
+  # config.username = 'bozo'                    # optional, but needed for user-related functions
+  # config.password = 'cl0wnt0wn'               # optional, but needed for user-related functions
+  # config.response_format = 'json'             # defaults to json, but xml is also supported
+  # config.logger = Logger.new('/dev/null')     # defaults to Rails.logger or Logger.new(STDOUT). Set to Logger.new('/dev/null') to disable logging.
 end
